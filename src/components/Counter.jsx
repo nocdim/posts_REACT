199f13806a737f9b55577fcac22c4ee6254dbe0a
nocdim <nokkudimu@gmail.com>
2021-11-23 19:01:06 +0300

    change

---
+++ src/components/Counter.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+const Counter = () => {
+    const [count, setCount] = useState(0)  //деструктизация useState (1 параметр - переменная, 2 - функция)
+
+    function increment(){
+        setCount(count + 1)
+    }
+
+    function decrement(){
+        setCount(count - 1)
+    }
+
+    return (
+        <div>
+            <h1>{count}</h1> {/* Передаем переменную count, которая = 0 по умолчанию */}
+            <button onClick={increment}>Increase</button>
+            <button onClick={decrement}>Decrease</button>
+        </div>
+    )
+}
+
+export default Counter
