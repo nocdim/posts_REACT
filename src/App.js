1a4792a594f5af2a254b9c1202546091e9e4b52c
nocdim <nokkudimu@gmail.com>
2021-11-25 23:51:26 +0300

    custom hook done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import PostForm from "./components/Postform";
 import Postlist from "./components/Postlist";
 import MyButton from "./components/UI/button/MyButton";
 import MyModal from "./components/UI/modal/MyModal";
+import { usePosts } from "./hooks/usePosts";
 
 
 //import ClassCounter from "./components/ClassCounter";
@@ -12,28 +13,18 @@ import './styles/App.css'
 
 function App() {
 
-  const [posts, setPosts] = useState([
-    { id: 1, title: 'yuh', body: 'xx' },
-    { id: 2, title: 'bruh', body: 'qq' },
-    { id: 3, title: 'naniu', body: 'jj' }
-  ])
+  const [posts, setPosts] = useState([])
 
   //реализуем двустороннее связывание / делаем компонент упарвляемым
   const [filter, setFilter] = useState({ sort: '', query: '' })
   const [modal, setModal] = useState(false)
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
 
   // хук useMemo
 
-  const sortedPosts = useMemo(() => {
-    if (filter.sort) {
-      return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
-    }
-    return posts
-  }, [filter.sort, posts])
+  
 
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()))
-  }, [filter.query, sortedPosts])
+  
 
 
   // const bodyInputRef = useRef() // способ получить данные из неуправляемого инпута
